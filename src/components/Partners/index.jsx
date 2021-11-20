9d59c32741f2be754deea8bc7fabc794f16a5cec
anhladrt <67309998+anhladrt@users.noreply.github.com>
2021-11-20 16:35:54 +0700

    remove logo

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -1,16 +1,16 @@
-import { Container, Row } from "react-bootstrap"
-import styled from "styled-components"
-import LabelSection from "../LabelSection"
-import logoDig from "../../assets/img/Logo/logoDig.svg"
-import logoDefi from "../../assets/img/Logo/logoDefi.svg"
-import logoAnother from "../../assets/img/Logo/logoAnother.svg"
-import logoSplin from "../../assets/img/Logo/logoSplin.svg"
-import logoHive from "../../assets/img/Logo/logoHive.svg"
-import logoN from "../../assets/img/Logo/logoN.svg"
-import logoVR from "../../assets/img/Logo/logoVR.svg"
-import logoBirt from "../../assets/img/Logo/logoBirt.svg"
-import logoBlockchain from "../../assets/img/Logo/logoBlockchain.svg"
-import logoEdso from "../../assets/img/Logo/logoEdso.svg"
+import { Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+import LabelSection from "../LabelSection";
+import logoDig from "../../assets/img/Logo/logoDig.svg";
+import logoDefi from "../../assets/img/Logo/logoDefi.svg";
+import logoAnother from "../../assets/img/Logo/logoAnother.svg";
+import logoSplin from "../../assets/img/Logo/logoSplin.svg";
+import logoHive from "../../assets/img/Logo/logoHive.svg";
+import logoN from "../../assets/img/Logo/logoN.svg";
+import logoVR from "../../assets/img/Logo/logoVR.svg";
+import logoBirt from "../../assets/img/Logo/logoBirt.svg";
+import logoBlockchain from "../../assets/img/Logo/logoBlockchain.svg";
+import logoEdso from "../../assets/img/Logo/logoEdso.svg";
 
 const StyledBlockLogo = styled(Row)`
   margin-bottom: 124px;
@@ -26,13 +26,13 @@ const StyledBlockLogo = styled(Row)`
       margin-bottom: 120px;
     }
   }
-`
+`;
 
 const StyledLogo = styled.img`
   max-width: ${(props) => props.width}px;
   max-height: ${(props) => props.height}px;
   margin-right: ${(props) => props.right}px;
-`
+`;
 const Partners = () => {
   return (
     <Container>
@@ -46,13 +46,13 @@ const Partners = () => {
         <StyledLogo width="260" height="128" src={logoSplin} right="60" />
         <StyledLogo width="477" height="76" src={logoHive} right="184" />
         <StyledLogo width="306" height="157" src={logoN} right="160" />
-        <StyledLogo width="197" height="197" src={logoVR} right="170" />
+        {/* <StyledLogo width="197" height="197" src={logoVR} right="170" /> */}
         <StyledLogo width="215" height="198" src={logoBirt} />
         <StyledLogo width="858" height="186" src={logoBlockchain} right="165" />
         <StyledLogo width="314" height="54" src={logoEdso} />
       </StyledBlockLogo>
     </Container>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
