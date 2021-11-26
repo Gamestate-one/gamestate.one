7d2201a43599b6dbc870205187dcf71b7a2f1814
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-26 13:48:05 +0700

    fix(TokenSale): increase hours fiveLogo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,9 +9,7 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
 import { GlobalStyle } from 'styles/global-styles';
-
 import HomePage from './pages/Home';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
