07fc216ea1bb15de75f705de5ca59616e1334de0
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-29 11:11:07 +0700

    fix(Hero): responsive loading video

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,36 +1,45 @@
-import styled from "styled-components";
-import { useState } from "react";
-import { SolarSystemLoading } from "react-loadingg";
+import styled from "styled-components"
+import { useState } from "react"
+import { SolarSystemLoading } from "react-loadingg"
 const Hero = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+  const StyledLoading = styled(SolarSystemLoading)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  `
   return (
     <Div>
-      {isLoading && <SolarSystemLoading color='yellow' />}
+      {isLoading && <StyledLoading color="yellow" />}
       <video
-        width='100%'
-        height='80%'
+        width="100%"
+        height="80%"
         autoPlay
         loop
         muted
         playsInline
-        preload='none'
+        preload="none"
         onLoadStart={() => {
-          setIsLoading(true);
+          setIsLoading(true)
         }}
         onCanPlay={() => {
-          setIsLoading(false);
+          setIsLoading(false)
         }}
       >
         <source
-          src='https://s3.ap-southeast-1.amazonaws.com/defiforyou.uk/Logo_Effect_VS1.mp4'
-          type='video/mp4'
+          src="https://s3.ap-southeast-1.amazonaws.com/defiforyou.uk/Logo_Effect_VS1.mp4"
+          type="video/mp4"
         />
       </video>
     </Div>
-  );
-};
-export default Hero;
+  )
+}
+export default Hero
 const Div = styled.div`
+  position: relative;
   text-align: center;
   display: flex;
   justify-content: center;
@@ -39,4 +48,4 @@ const Div = styled.div`
   @media screen and (max-width: 991px) {
     padding-bottom: 30px;
   }
-`;
+`
